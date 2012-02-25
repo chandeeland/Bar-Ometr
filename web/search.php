f6bfd426e57e28e5478db6642c691c2509987dcb
chandeeland <chand@chandeeland.org>
2012-02-25 17:23:33 -0500

    search working

--- web/search.php
+++ web/search.php
@@ -1,16 +1,25 @@
+
+<html>
+<head>
+<title>search</title>
+</head>
+
+<body>
+
 <?php
-require('scraper/foursquare.php');
+require('../classes/scraper/foursquare.php');
 
 if ($q = $_REQUEST['q']) {
     $scrape = new foursquare();
-    $venue = $scrape->search_venue('40.7,-74', $_REQUEST['q'], 10);
+    $venues = $scrape->search_venue('40.7,-74', $_REQUEST['q'], 10);
 
     foreach($venues as $k => $v) {
         $name = $v['name'];
-        $cat =  $v['categories'][0]['name'];
-        $pics = urlencode(serialize($scrape->get_photos($venue[$k]['id'])));
+	if (isset($v['categories'][0]))
+       	 	$cat =  $v['categories'][0]['name'];
+        $pics = urlencode(serialize($scrape->get_photos($venues[$k]['id'])));
         
-        echo "<a href=\"/report.php?v=$pics\">$name ($cat) </a>\n";
+        echo "<li><a href=\"/report.php?v=$pics\">$name ($cat) </a>\n";
     }
 
 
@@ -18,12 +27,6 @@ if ($q = $_REQUEST['q']) {
     exit;
 }     
 ?>
-<html>
-<head>
-<title>search</title>
-</head>
-
-<body>
     <form action='/search.php' method="post">
         Fine a venue nearby...<br>
         <input type="text" name="q" />
