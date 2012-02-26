d3152b7082d00176655bca6df4514c611f0358a2
David Chan <chand@chandeeland.org>
2012-02-26 11:12:52 -0500

    typo

--- classes/reporter.php
+++ classes/reporter.php
@@ -133,7 +133,8 @@ class venue {
         $badges = array();
         if ($this->getRatio() < 0.5 and count($this->people) > 4) {
             $badges[] = 'Sausage Fest';
-            if (array_key_exists('sad', $pm) && array_key_exists('surprise', $pm))
+            
+            if (array_key_exists('sad', $pm) && array_key_exists('surprised', $pm))
             if ($pm['sad'] + $pm['surprised'] > 0.50) {
                 $badges[] = 'Lots of sadness, lots of surprise, are these guys coding?';
             }
