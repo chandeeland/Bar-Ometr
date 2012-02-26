834ae785cff04535ee47896c21289ab62b9870e1
Rodrigo Sanchez <flea@test-users-MacBook-Pro.local>
2012-02-26 04:07:10 -0500

    removed row class from wrapper

--- web/report.php
+++ web/report.php
@@ -154,7 +154,7 @@ function pie_chart($title, $slices, $container = 'container') {
        chromium.org/developers/how-tos/chrome-frame-getting-started -->
   <!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
   
- 	<div class="row wrapper">
+ 	<div class="wrapper">
 	
 		<header>
 			<h1 class="ir"><a href=""><?= $qname; ?></a></h1>
