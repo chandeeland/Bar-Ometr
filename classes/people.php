37ce8bb12fbf7d1af1273e87ca6d296db4769e94
David Chan <chand@chandeeland.org>
2012-02-26 10:05:15 -0500

    fixed math around people heads

--- classes/people.php
+++ classes/people.php
@@ -38,9 +38,16 @@ class Person {
         }
 
         $this->image_url = $p->url;
-        $this->top = round($p->center->y - ($p->height / 2));
-        $this->bottom = round($p->center->y + ($p->height / 2));
-        $this->left = round($p->center->x - ($p->width / 2));
-        $this->right = round($p->center->x - ($p->width / 2));
+        
+        $centerx = $p->total_width * $p->center->x * .01;
+        $centery = $p->total_height * $p->center->y * .01;
+        $h = $p->total_height * $p->height * .01;
+        $w = $p->total_width * $p->width * .01;
+
+
+        $this->top = round($centery - ($h));
+        $this->bottom = round($centery + ($h ));
+        $this->left = round($centerx - ($w  ));
+        $this->right = round($centerx + ($w ));
     }
 }
