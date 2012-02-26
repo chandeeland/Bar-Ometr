3eeed8aca385b4ab4ec2fc8daee0897339b3a92d
Rodrigo Sanchez <flea@test-users-MacBook-Pro.local>
2012-02-25 20:07:42 -0500

    removing html

--- web/index.php
+++ web/index.php
@@ -55,7 +55,7 @@
        chromium.org/developers/how-tos/chrome-frame-getting-started -->
   <!--[if lt IE 7]><p class=chromeframe>Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</p><![endif]-->
   
- 	<div class="row wrapper">
+ 	<div class="wrapper">
 	
 		<header>
 			<h1 class="ir"><a href="">Co Name Here</a></h1>
